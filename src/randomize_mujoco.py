008ff653e858cad16a03ccbb915aa68fca808b28
jamestkpoon <jamestkpoon@gmail.com>
2019-04-17 13:31:31 +0900

    rando

--- src/randomize_mujoco.py
+++ src/randomize_mujoco.py
@@ -1,55 +1,46 @@
 #!/usr/bin/env python
 
 import rospy
+from mujoco_ros.srv import *
 from std_srvs.srv import Empty,EmptyRequest
 
-import numpy as np
-
-
-
-def gen_key_str(mean, noise):
-    return 'nug'
 
 
 if __name__ == '__main__':
 
     rospy.init_node('mujoco_randomizer', anonymous=True)
 
-    #rospy.wait_for_service('/mujoco/reset')
-    #reset_svc_ = rospy.ServiceProxy('/mujoco/reset', Empty)
-
-    # randomization parameter dicts
-    params_io_ = rospy.get_param('io')
-    params_v_ = rospy.get_param('visual')
-    #params_p_ = rospy.get_param('physical')
-
-    print(params_v_)
+    # randomization parameters
+    params_tex_ = rospy.get_param('textural')
+    params_phys_ = rospy.get_param('physical')
+    
+    tex_noise_flat_ = []
+    for k in params_tex_.keys(): tex_noise_flat_ += params_tex_[k]
+    phys_noise_flat_ = []
+    for k in params_phys_.keys(): phys_noise_flat_ += params_phys_[k]
+    
+    
+    
+    # randomization services
+    rospy.wait_for_service('textural')
+    rand_tex_svc_ = rospy.ServiceProxy('textural', RandomizeTexturalAttribute)
+    rospy.wait_for_service('physical')
+    rand_phys_svc_ = rospy.ServiceProxy('physical', RandomizePhysicalAttribute)
+    
+    
 
     def randomize_cb(req):
-        
-        # load base file
-        with open(params_io_['template_fp'], 'r') as f:
-            data_ = f.read()
-            
-        # make visual changes
-        for i in range(len(params_v_['keys'])):
-            str_ = gen_key_str(params_v_['mean'][i], params_v_['noise'][i])
-#            data_ = data_.replace(params_v_['keys'][i], str_)
-            
-        # make textural changes
-        
-        # save randomized file
-        with open(params_io_['rand_fp'], 'w') as f:
-            f.write(data_)
-        
-        # reset sim to apply new .xml
-    #    reset_svc_(EmptyRequest())
+        # textural
+        rand_tex_svc_(RandomizeTexturalAttributeRequest(
+            material_names=params_tex_.keys(), noise=tex_noise_flat_))
+        # physical
+        rand_phys_svc_(RandomizePhysicalAttributeRequest(
+            body_names=params_phys_.keys(), noise=phys_noise_flat_))
         
         return []
         
-        
-
-    #rospy.Service('/mujoco/randomize', Empty, randomize_cb)
-
-    #rospy.spin()
-    randomize_cb(EmptyRequest())
+    
+       
+    rospy.Service('randomize', Empty, randomize_cb)
+    
+    rospy.spin()
--- src/thread_lock_watchdog.py
+++ src/thread_lock_watchdog.py
@@ -4,6 +4,7 @@ import rospy
 from tf.transformations import euler_from_quaternion
 from mujoco_ros.srv import *
 
+import sys
 import numpy as np
 
 
@@ -42,12 +43,14 @@ if __name__ == '__main__':
 
     rospy.init_node('threaded_watchdog_node', anonymous=True)
     
-    manip_params_ = rospy.get_param('')
+    fastener_name_ = sys.argv[1]
+    manip_params_ = rospy.get_param(fastener_name_)
+    pair_str_ = "'{}' and '{}'".format(manip_params_['attach_name'],fastener_name_)
     connected_ = False
     
-    rospy.wait_for_service('/mujoco/get_relative_pose_bodies')
-    obj_pose_svc_ = rospy.ServiceProxy('/mujoco/get_relative_pose_bodies', GetRelativePoseBodies)
-    thread_lock_svc_ = rospy.ServiceProxy('/mujoco/thread_lock', ThreadLock)
+    rospy.wait_for_service('get_relative_pose_bodies')
+    obj_pose_svc_ = rospy.ServiceProxy('get_relative_pose_bodies', GetRelativePoseBodies)
+    thread_lock_svc_ = rospy.ServiceProxy('thread_lock', ThreadLock)
 
     rospy.sleep(1.0)
     
@@ -56,7 +59,7 @@ if __name__ == '__main__':
     while True:
         # get relative pose
         pose_res_ = obj_pose_svc_(GetRelativePoseBodiesRequest(
-            bodyA_name=manip_params_['attach_name'], bodyB_name=manip_params_['fastener_name']))
+            bodyA_name=manip_params_['attach_name'], bodyB_name=fastener_name_))
               
         if pose_res_.ok:
             # check relative pose for alignment criteria
@@ -64,17 +67,17 @@ if __name__ == '__main__':
                 manip_params_['attach_axis'], manip_params_['attach_axis_linear_offset'],
                 manip_params_['attach_pos_thresh'], manip_params_['attach_eul_thresh'])
                 
-            if not connected_ and tchk_: # lock
-                tl_ok_ = thread_lock_svc_(ThreadLockRequest(
-                    fastener_name=manip_params_['fastener_name'], attach_flag=True,
+            if not connected_ and tchk_: # connect
+                connected_ = thread_lock_svc_(ThreadLockRequest(
+                    fastener_name=fastener_name_, attach_flag=True,
                     axis=manip_params_['attach_axis'], pitch=manip_params_['pitch'])).ok
-                connected_ = True
-                print("  Threaded '%s' and '%s'" % (manip_params_['attach_name'], manip_params_['fastener_name']))
-            elif connected_ and not tchk_: # unlock
-                tl_ok_ = thread_lock_svc_(ThreadLockRequest(
-                    fastener_name=manip_params_['fastener_name'], attach_flag=False,
-                    axis=manip_params_['attach_axis'], pitch=manip_params_['pitch'])).ok
-                connected_ = False
-                print("  Un-threaded '%s' and '%s'" % (manip_params_['attach_name'], manip_params_['fastener_name']))
+                if(connected_): print('  Connected %s' % pair_str_)
+                else: print('  Failed to connect %s' % pair_str_)
+            elif connected_ and not tchk_: # disconnect
+                dc_ok_ = thread_lock_svc_(ThreadLockRequest(
+                    fastener_name=fastener_name_, attach_flag=False)).ok
+                connected_ = not dc_ok_
+                if(dc_ok_): print('  Disconnected %s' % pair_str_)
+                else: print('  Failed to disconnect %s' % pair_str_)
         
         rospy.sleep(0.1)
